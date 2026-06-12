089bbd780e57d73006b558758c6defa3118fd594
Amit <amit.websitedev@gmail.com>
2026-06-12 15:24:10 +0530

    Update codebase to point to new Supabase project sizvcdcdcrodxyfbqkau

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 // Initialize Supabase Client
-const SUPABASE_URL = 'https://yqoyweqarzezcvdcgnzk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlxb3l3ZXFhcnplemN2ZGNnbnprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2MjI2MTksImV4cCI6MjA4NTE5ODYxOX0.4Z5y5oyh1thfmRgUUBAFkBFmyy4Y_8QED3P_UZDovB8';
+const SUPABASE_URL = 'https://sizvcdcdcrodxyfbqkau.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpenZjZGNkY3JvZHh5ZmJxa2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNDcwODYsImV4cCI6MjA5NjgyMzA4Nn0.26EVSOXBzn8XfsNYefz1LpUvQkL5p70dKpYDePa3mGI';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
